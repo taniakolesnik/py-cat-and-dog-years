4e66999aaa86d15e5056844279fcd284ee8341a3
Tania Kolesnik <kolesnik@gmail.com>
2025-10-25 09:29:00 +0100

    add solution 3

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError
+
+    if not isinstance(cat_age, int) or not isinstance(cat_age, int):
+        raise TypeError
+
     cat_to_human = convert_to_human(cat_age, 15, 9, 4)
     dog_to_human = convert_to_human(dog_age, 15, 9, 5)
     return [cat_to_human, dog_to_human]
--- app/test_main.py
+++ app/test_main.py
@@ -13,9 +13,7 @@ class TestMain:
             (24, 24, [2, 2]),
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
-            (100, 100, [21, 17]),
-            (-15, 15, [1, 1]),
-            (15, -15, [1, 1]),
+            (100, 100, [21, 17])
         ]
     )
     def test_human_age(
@@ -25,3 +23,21 @@ class TestMain:
             expected: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
+
+
+class TestExceptions:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_exception",
+        [
+            (-15, -15, ValueError),
+            ("15", "15", TypeError)
+        ]
+    )
+    def test_human_age(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_exception: type
+    ) -> None:
+        with pytest.raises(expected_exception):
+            get_human_age(cat_age, dog_age)
